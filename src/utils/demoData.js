0edc0684dad74bd92d70249038da2828ff8bb955
Lalit Bhausar <lbhausar@nvidia.com>
2026-05-17 13:12:32 +0530

    fix chole bhature image

--- src/utils/demoData.js
+++ src/utils/demoData.js
@@ -57,7 +57,7 @@ export const demoRestaurants = [
     id: 'rest4',
     name: 'Chacha Ji Ke Chole Bhature',
     nameHi: 'चाचा जी के छोले भटूरे',
-    image: 'https://images.unsplash.com/photo-1601050690597-df0568f70950?w=400&h=250&fit=crop',
+    image: 'https://images.unsplash.com/photo-1626132647523-66f5bf380027?w=400&h=250&fit=crop',
     cuisine: ['Street Food', 'North Indian'],
     rating: 4.6,
     totalRatings: 450,
